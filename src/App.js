beca94e44f115d5a74fcfcbcce63c0b09a5ab604
lmorales <lmorales@thefactoryhka.com>
2023-01-02 10:28:35 -0600

    Uso de hook de redux para obtener informacion de API

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { useEffect, useState } from 'react';
-import { connect } from 'react-redux';
+import { useEffect } from 'react';
 import { Col } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 import Searcher from './components/Searcher';
 import PokemonList from './components/PokemonList';
 import { getPokemon } from './api/index';
-import { setPokemons as setPokemonsActions} from './actions';
+import { setPokemons } from './actions';
 import logo from './statics/logo.svg';
 import './App.css';
 
 
-function App({pokemons, setPokemons}) {
+function App() {
 
-  //Uso del state para el manejos de la lista de pokemons
-  //const [pokemons, setPokemons]= useState([]);
-  console.log("🚀 ~ file: App.js:16 ~ App ~ pokemons", pokemons);
-  
+  //Uso de useSelector
+  const pokemons = useSelector(state => state.pokemons);
+  const dispatch = useDispatch();
 
   //Hacemos uso del Hook de useEffect para obtener los datos de nustra api
   useEffect(() => {
     const fetchPokemons = async () => {
       const pokemonsRes = await getPokemon();
 
-      setPokemons(pokemonsRes);
+      dispatch(setPokemons(pokemonsRes));
     };
 
     fetchPokemons();
@@ -40,14 +39,4 @@ function App({pokemons, setPokemons}) {
   );
 }
 
-//Funcion que va a recibir nuestro estado y va a retornar un objeto
-//cuyas propiedades van a ser enviadas a los props del componente que se esta conectando a redux
-const mapStateToProps = (state) => ({
-  pokemons: state.pokemons,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  setPokemons: (value) => dispatch(setPokemonsActions(value)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
